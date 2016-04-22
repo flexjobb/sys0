6729f02b7654a088378a9b99dbc2363825e6bfa9
flexjobb <flexjobb@gmail.com>
2016-04-22 13:08:07 +0200

    Create Test2.java

---
+++ sys0/src/sys0/Test2.java
@@ -0,0 +1,20 @@
+/**
+ * Test2
+ */
+package sys0;
+
+/**
+ * @author flex tex mex 2
+ *
+ */
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Text Mex 2");
+	}
+
+}
