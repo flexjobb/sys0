ed792b4cae9b1b207febfc7272750d36e78f8849
flexjobb <flexjobb@gmail.com>
2016-04-22 13:04:36 +0200

    test

---
+++ sys0/src/sys0/Test.java
@@ -0,0 +1,20 @@
+/**
+ * Test
+ */
+package sys0;
+
+/**
+ * @author flex tex mex
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
